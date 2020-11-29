dc827ebcc200809607e9bfed7e2afb3abb75f8c6
sderam95 <42761276+sderam95@users.noreply.github.com>
2020-11-29 15:06:46 +0530

    Add files via upload

---
+++ build/precache-manifest.56f62b3c2eb3457b03f127c5cb6d582d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "270e8fdc9ec3e50477fdd39556580d2f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "17f74b2ba5f20c426d98",
+    "url": "/portfolio/static/css/main.37cbe82b.chunk.css"
+  },
+  {
+    "revision": "b6fdec40b3bc54e7d9dd",
+    "url": "/portfolio/static/js/2.77a6480b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/portfolio/static/js/2.77a6480b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17f74b2ba5f20c426d98",
+    "url": "/portfolio/static/js/main.0be9cd4c.chunk.js"
+  },
+  {
+    "revision": "4c76482b968eb9f47af6",
+    "url": "/portfolio/static/js/runtime-main.98fb296d.js"
+  }
+]);
